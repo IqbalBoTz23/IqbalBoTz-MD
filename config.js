5a3be5231b6b3d7e869318376377145e67eb03d0
IqbalBoTz23 <123338342+IqbalBoTz23@users.noreply.github.com>
2023-01-31 14:48:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ let chalk = require('chalk')
 
 owner = [
   ['6283155609158'],
-  ['6283155609158'],
-  ['62895330379186', 'Owner', true]
+  ['6283155609186'],
+  ['6283155609158', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 mods = [] // Want some help?
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@ainee_bot'
-  var sticker_author = 'Aine'
+  var sticker_name = '@𝙄𝙦𝙗𝙖𝙡 𝙁𝙖𝙪𝙯𝙞'
+  var sticker_author = '𝙄𝙦𝙗𝙖𝙡 𝙁𝙖𝙪𝙯𝙞'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 packname = sticker_name
 author = sticker_author
-wm = '© Aine'
+wm = '© 𝙄𝙦𝙗𝙖𝙡 𝙁𝙖𝙪𝙯𝙞'
 
 Intervalmsg = 1800 //detik
 
